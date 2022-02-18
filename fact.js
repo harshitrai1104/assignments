d2081c8d2d4f88bc7f43204aa82c6356cc5fa955
harshitrai1104 <60356851+harshitrai1104@users.noreply.github.com>
2022-02-18 12:30:15 +0530

    Update fact.js

--- fact.js
+++ fact.js
@@ -1,8 +1,9 @@
+// calculates the factorial of a given number
 function fact(n)
 {
   if(n) return  n*fact(n-1);
   else return 1;
 }
 
-console.log("Factorial of 10 is "+fact(10)
+console.log("Factorial of 10 is "+fact(10));
     
